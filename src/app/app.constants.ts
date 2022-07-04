555617bf1556a6d600cb833c7323f3ec90087e67
MladenKrsmanovic <mladen0krsmanovic@gmail.com>
2022-07-04 14:47:50 +0200

    Poslednji frontend commit

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,14 @@
-export const NACIONALNOST_URL = 'http://localhost:8083/nacionalnost';
-export const LIGA_URL = 'http://localhost:8083/liga';
-export const TIM_URL = 'http://localhost:8083/tim';
-export const IGRAC_URL = 'http://localhost:8083/igrac';
-export const IGRAC_ZATIM_URL ='http://localhost:8083/igracZaTimID';
+// export const NACIONALNOST_URL = 'http://localhost:8083/nacionalnost';
+// export const LIGA_URL = 'http://localhost:8083/liga';
+// export const TIM_URL = 'http://localhost:8083/tim';
+// export const IGRAC_URL = 'http://localhost:8083/igrac';
+// export const IGRAC_ZATIM_URL ='http://localhost:8083/igracZaTimID';
+
+
+export const NACIONALNOST_URL = 'https://backendmladen.herokuapp.com/nacionalnost';
+export const LIGA_URL = 'https://backendmladen.herokuapp.com/liga';
+export const TIM_URL = 'https://backendmladen.herokuapp.com/tim';
+export const IGRAC_URL = 'https://backendmladen.herokuapp.com/igrac';
+export const IGRAC_ZATIM_URL ='https://backendmladen.herokuapp.com/igracZaTimID';
+
+
